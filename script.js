6a69b16fc75fe0ce68f4e93268024874d926f5ec
hitesh Suthar <104884942+hitesh5444@users.noreply.github.com>
2023-06-12 11:50:26 +0530

    Add files via upload
    install fontawsome-4.0.7 file before exicute web view....

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("close").onclick = function () {
+    document.getElementById("alart").classList.toggle ("show")
+}
+
+document.getElementById("show").onclick = function () {
+    document.getElementById("alart").classList.toggle ("show")
+}
